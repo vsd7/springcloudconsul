9b4a895f2ad8d6330797cd9bf834fd5266a1656a
Vivek S Deshmukh <vsdrocks467@gmail.com>
2019-12-12 23:40:33 +0530

    initial commit

---
+++ springcloudconsul/src/main/java/com/vd/springcloudconsul/SpringcloudconsulApplication.java
@@ -0,0 +1,13 @@
+package com.vd.springcloudconsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudconsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudconsulApplication.class, args);
+	}
+
+}
---
+++ springcloudconsul/src/test/java/com/vd/springcloudconsul/SpringcloudconsulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vd.springcloudconsul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudconsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
